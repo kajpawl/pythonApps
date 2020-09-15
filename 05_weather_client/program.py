5983848bdb5065a9280ab9ed0c39b062838abf60
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-15 17:39:58 +0200

    Use BS4 to get html

--- 05_weather_client/program.py
+++ 05_weather_client/program.py
@@ -1,11 +1,16 @@
+import requests
+import bs4
+
+
 def main():
     print_header()
 
     country_code = input('What country do you want the weather for? (PL) ').lower()
     city = input('What city do you want the weather for? (Katowice) ').lower()
 
-    get_html_from_web(country_code, city)
+    html = get_html_from_web(country_code, city)
 
+    get_weather_from_html(html)
     # get html from web
     # parse the html
     # display the forecast
@@ -20,8 +25,13 @@ def print_header():
 
 def get_html_from_web(country_code, city):
     url = 'https://www.wunderground.com/weather/{}/{}'.format(country_code, city)
-    print(url)
-    requests
+    response = requests.get(url)
+    return response.text
+
+
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    print(soup)
 
 
 if __name__ == '__main__':
