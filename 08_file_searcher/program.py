d080ef88001fc56b5d350cf4d093238a34557a87
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-17 23:11:42 +0200

    Simple search works

--- 08_file_searcher/program.py
+++ 08_file_searcher/program.py
@@ -13,7 +13,9 @@ def main():
         print('Sorry, we can\'t search nothing!')
         return
 
-    search_folders(folder, text)
+    matches = search_folders(folder, text)
+    for match in matches:
+        print(match)
 
 
 def print_header():
@@ -35,11 +37,34 @@ def get_folder_from_user():
 
 def get_search_text_from_user():
     text = input('What are you searching for [single phrases only]? ')
-    return text
+    return text.lower()
 
 
 def search_folders(folder, text):
-    print('Would search {} for {}.'.format(folder, text))
+
+    all_matches = []
+    items = os.listdir(folder)
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            continue
+
+        matches = search_file(full_item, text)
+        all_matches.extend(matches)
+
+    return all_matches
+
+
+def search_file(filename, search_text):
+    matches = []
+    with open(filename, 'r', encoding='utf-8') as file_in:
+
+        for line in file_in:
+            if line.lower().find(search_text) >= 0:
+                matches.append(line)
+
+        return matches
 
 
 if __name__ == '__main__':
