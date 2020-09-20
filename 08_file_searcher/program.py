0360abb3320de6c63214657d402ef21bc2db6dd0
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-20 21:50:04 +0200

    Remove comments

--- 08_file_searcher/program.py
+++ 08_file_searcher/program.py
@@ -24,11 +24,6 @@ def main():
         print('match: ' + match.text.strip())
         print()
 
-    # match_count = 0
-    # for m in matches:
-        # match_count += 1
-    # print('Fount {:,} matches.'.format(match_count))
-
 
 def print_header():
     print('-----------------------------------')
@@ -54,31 +49,17 @@ def get_search_text_from_user():
 
 
 def search_folders(folder, text):
-    # all_matches = []
     items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
-            # matches = search_folders(full_item, text)
-            # all_matches.extend(matches)
-
-            # for match in matches:
-            #     yield match
             yield from search_folders(full_item, text)
         else:
-            # matches = search_file(full_item, text)
-            # all_matches.extend(matches)
-
-            # for match in matches:
-            #     yield match
             yield from search_file(full_item, text)
-
-    # return all_matches
 
 
 def search_file(filename, search_text):
-    # matches = []
     with open(filename, 'r', encoding='utf-8') as file_in:
 
         line_num = 0
@@ -86,10 +67,7 @@ def search_file(filename, search_text):
             line_num += 1
             if line.lower().find(search_text) >= 0:
                 match = SearchResult(line=line_num, file=filename, text=line)
-                # matches.append(match)
                 yield match
-
-        # return matches
 
 
 if __name__ == '__main__':
