19306e4576f42127e6b8ac6bc613fc820f063317
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-20 21:48:11 +0200

    Create generator methods for searching

--- 08_file_searcher/program.py
+++ 08_file_searcher/program.py
@@ -54,24 +54,31 @@ def get_search_text_from_user():
 
 
 def search_folders(folder, text):
-
-    all_matches = []
+    # all_matches = []
     items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
-            matches = search_folders(full_item, text)
-            all_matches.extend(matches)
-        else:
-            matches = search_file(full_item, text)
-            all_matches.extend(matches)
+            # matches = search_folders(full_item, text)
+            # all_matches.extend(matches)
 
-    return all_matches
+            # for match in matches:
+            #     yield match
+            yield from search_folders(full_item, text)
+        else:
+            # matches = search_file(full_item, text)
+            # all_matches.extend(matches)
+
+            # for match in matches:
+            #     yield match
+            yield from search_file(full_item, text)
+
+    # return all_matches
 
 
 def search_file(filename, search_text):
-    matches = []
+    # matches = []
     with open(filename, 'r', encoding='utf-8') as file_in:
 
         line_num = 0
@@ -79,9 +86,10 @@ def search_file(filename, search_text):
             line_num += 1
             if line.lower().find(search_text) >= 0:
                 match = SearchResult(line=line_num, file=filename, text=line)
-                matches.append(match)
+                # matches.append(match)
+                yield match
 
-        return matches
+        # return matches
 
 
 if __name__ == '__main__':
