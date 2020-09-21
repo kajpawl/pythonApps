daf6e931a07d287a5d559bde458c663ae63215e9
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-21 17:41:53 +0200

    Get average prices with loops

--- 09_real_estate_app/program.py
+++ 09_real_estate_app/program.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import statistics
 
 from data_types import Purchase
 
@@ -71,8 +72,21 @@ def query_data(data):
         low_purchase.price, low_purchase.beds, low_purchase.baths))
 
     # average price house?
+    prices = []
+    for purchase in data:
+        prices.append(purchase.price)
+
+    average_price = statistics.mean(prices)
+    print('The average home price is ${:,}'.format(int(average_price)))
+
     # average price of 2 bedroom houses
-    pass
+    prices = []
+    for purchase in data:
+        if purchase.beds == 2:
+            prices.append(purchase.price)
+
+    average_price = statistics.mean(prices)
+    print('The average price of a 2-bedroom home is ${:,}'.format(int(average_price)))
 
 
 if __name__ == '__main__':
