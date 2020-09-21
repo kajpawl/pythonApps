a2f4726259d2a16a1e442a134a47977c6e28018f
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-21 17:16:54 +0200

    Switch function to lambda expression

--- 09_real_estate_app/program.py
+++ 09_real_estate_app/program.py
@@ -51,22 +51,24 @@ def load_file(filename):
 #         print(lines[:5])
 
 
-def get_price(p):
-    return p.price
+# def get_price(p):
+#     return p.price
 
 
 def query_data(data):
-
     # if data was sorted by price:
-    data.sort(key=get_price)
+    # data.sort(key=get_price)
+    data.sort(key=lambda p: p.price)
 
     # most expensive house?
     high_purchase = data[-1]
-    print(high_purchase.price)
+    print('The most expensive house is ${:,} with {} beds and {} baths'.format(
+        high_purchase.price, high_purchase.beds, high_purchase.baths))
 
     # least expensive house?
     low_purchase = data[0]
-    print(low_purchase.price)
+    print('The least expensive house is ${:,} with {} beds and {} baths'.format(
+        low_purchase.price, low_purchase.beds, low_purchase.baths))
 
     # average price house?
     # average price of 2 bedroom houses
