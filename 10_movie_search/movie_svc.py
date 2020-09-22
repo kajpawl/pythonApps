c54ddb47a494204bc5d8360e8c46bed470c56a6f
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-22 16:06:02 +0200

    Refactor program file, extract movie service

---
+++ 10_movie_search/movie_svc.py
@@ -0,0 +1,22 @@
+import requests
+import collections
+
+MovieResult = collections.namedtuple('MovieResult',
+                                     'imdb_code, title, director, keywords, duration, genres, rating, year, imdb_score')
+
+
+def find_movies(search_text):
+    url = 'http://movieservice.talkpython.fm/api/search/{}'.format(search_text)
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    movie_data = resp.json()
+    movies_list = movie_data.get('hits')
+
+    movies = [
+        MovieResult(**md)
+        for md in movies_list
+    ]
+
+    return movies
--- 10_movie_search/program.py
+++ 10_movie_search/program.py
@@ -1,14 +1,9 @@
-import requests
-import collections
-
-MovieResult = collections.namedtuple('MovieResult',
-                                     'imdb_code, title, director, keywords, duration, genres, rating, year, imdb_score')
+import movie_svc
 
 
 def main():
     print_header()
-    search_text = get_search_text()
-    get_movies(search_text)
+    search_event_loop()
 
 
 def print_header():
@@ -18,47 +13,19 @@ def print_header():
     print()
 
 
-def get_search_text():
-    return input('Enter search phrase (movie title): ').lower()
+def search_event_loop():
+    search_text = 'ONCE_THROUGH_LOOP'
 
+    while search_text != 'x':
+        search_text = input('Movie search text (x to exit): ').lower()
+        if search_text != 'x':
+            results = movie_svc.find_movies(search_text)
+            print('Found {} movies for search {}.'.format(len(results), search_text))
+            for r in results:
+                print("{} -- {}".format(r.year, r.title))
+            print()
 
-def get_movies(search_text):
-    url = 'http://movieservice.talkpython.fm/api/search/{}'.format(search_text)
-
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    movie_data = resp.json()
-    movies_list = movie_data.get('hits')
-
-    # movies = []
-    # for md in movies_list:
-    #     m = MovieResult(
-    #         imdb_code=md.get('imdb_code'),
-    #         title=md.get('title'),
-    #         director=md.get('director'),
-    #         keywords=md.get('keywords'),
-    #         duration=md.get('duration'),
-    #         genres=md.get('genres'),
-    #         rating=md.get('rating', 0),
-    #         year=md.get('year', 0),
-    #         imdb_score=md.get('imdb_score', 0.0)
-    #     )
-    #     movies.append(m)
-
-    # movies = []
-    # for md in movies_list:
-    #     m = MovieResult(**md)
-    #     movies.append(m)
-
-    movies = [
-        MovieResult(**md)
-        for md in movies_list
-    ]
-
-    print('Found {} movies for search {}.'.format(len(movies), search_text))
-    for m in movies:
-        print("{} -- {}".format(m.year, m.title))
+    print('Exiting...')
 
 
 if __name__ == '__main__':
