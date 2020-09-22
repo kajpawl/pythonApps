dde620db57d6197a7ba757fec903b8ef208f9b8f
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-09-22 15:51:18 +0200

    Create named tuples with kwargs

--- 10_movie_search/program.py
+++ 10_movie_search/program.py
@@ -31,22 +31,26 @@ def get_movies(search_text):
     movie_data = resp.json()
     movies_list = movie_data.get('hits')
 
+    # movies = []
+    # for md in movies_list:
+    #     m = MovieResult(
+    #         imdb_code=md.get('imdb_code'),
+    #         title=md.get('title'),
+    #         director=md.get('director'),
+    #         keywords=md.get('keywords'),
+    #         duration=md.get('duration'),
+    #         genres=md.get('genres'),
+    #         rating=md.get('rating', 0),
+    #         year=md.get('year', 0),
+    #         imdb_score=md.get('imdb_score', 0.0)
+    #     )
+    #     movies.append(m)
+
     movies = []
     for md in movies_list:
-        m = MovieResult(
-            imdb_code=md.get('imdb_code'),
-            title=md.get('title'),
-            director=md.get('director'),
-            keywords=md.get('keywords'),
-            duration=md.get('duration'),
-            genres=md.get('genres'),
-            rating=md.get('rating', 0),
-            year=md.get('year', 0),
-            imdb_score=md.get('imdb_score', 0.0)
-        )
+        m = MovieResult(**md)
         movies.append(m)
 
-    # print(type(movie_data))
     print('Found {} movies for search {}.'.format(len(movies), search_text))
     for m in movies:
         print("{} -- {}".format(m.year, m.title))
